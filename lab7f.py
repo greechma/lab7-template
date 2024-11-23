3c1eddf7f7d6bebc7ccc588d269e6353144dd7b0
Grishma Gairhe <ggairhe@myseneca.ca>
2024-11-23 00:31:53 -0500

    Lab 7 completed

---
+++ lab7f.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#student ID: ggairhe
+
+class Time:
+    def __init__(self, hour=12, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def format_time(self):
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def sum_times(self, t2):
+        sum = Time(0, 0, 0)
+        sum.hour = self.hour + t2.hour
+        sum.minute = self.minute + t2.minute
+        sum.second = self.second + t2.second
+
+        while sum.second >= 60:
+            sum.second -= 60
+            sum.minute += 1
+
+        while sum.minute >= 60:
+            sum.minute -= 60
+            sum.hour += 1
+
+        return sum
+
+    def change_time(self, seconds):
+        time_seconds = self.time_to_sec()
+        nt = sec_to_time(time_seconds + seconds)
+        self.hour, self.minute, self.second = nt.hour, nt.minute, nt.second
+        return None
+
+    def time_to_sec(self):
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+
+    def valid_time(self):
+        if self.hour < 0 or self.minute < 0 or self.second < 0:
+            return False
+        if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
+            return False
+        return True
+
+    def __str__(self):
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+    def __add__(self, t2):
+        return self.sum_times(t2)
+
+def sec_to_time(seconds):
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
